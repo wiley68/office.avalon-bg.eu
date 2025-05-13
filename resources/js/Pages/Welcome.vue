e7c112cbb3579e82af068124f33dea95d6de466b
wiley68 <ilko.iv@gmail.com>
2025-05-13 08:46:21 +0300

    change Welcome.vue page

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -8,20 +8,27 @@ defineProps({
 </script>
 
 <template>
-
   <Head title="Welcome"></Head>
 
   <div
-    class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-center bg-gray-100 dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+    class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-center bg-gray-100 dark:bg-gray-900 selection:bg-red-500 selection:text-white"
+  >
     <div v-if="canLogin" class="sm:fixed sm:top-0 sm:end-0 p-6 text-end z-10">
-      <Link v-if="$page.props.auth.user" :href="route('dashboard')"
-        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
-      Dashboard</Link>
+      <Link
+        v-if="$page.props.auth.user"
+        :href="route('dashboard')"
+        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+      >
+        Dashboard</Link
+      >
 
       <template v-else>
-        <Link :href="route('login')"
-          class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
-        Log in</Link>
+        <Link
+          :href="route('login')"
+          class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+        >
+          Log in</Link
+        >
 
         <!-- <Link
           v-if="canRegister"
@@ -33,8 +40,12 @@ defineProps({
     </div>
 
     <div class="max-w-7xl mx-auto p-6 lg:p-8">
-      <div class="flex justify-center mt-16 px-6 sm:items-center sm:justify-between">
-        <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-end sm:ms-0">
+      <div
+        class="flex justify-center mt-16 px-6 sm:items-center sm:justify-between"
+      >
+        <div
+          class="text-center text-3xl text-gray-500 dark:text-gray-400 sm:text-end sm:ms-0"
+        >
           Avalon
         </div>
       </div>
